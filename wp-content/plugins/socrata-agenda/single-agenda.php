d3e04c50f925a5299618dcf80d07d7c73240107c
Michael Church <michael@fishinglounge.com>
2017-02-16 15:32:03 -0500

    progress dev

--- wp-content/plugins/socrata-agenda/single-agenda.php
+++ wp-content/plugins/socrata-agenda/single-agenda.php
@@ -1,6 +1,6 @@
 <?php 
+$jobtitle = rwmb_meta( 'agenda_speakers','',$speaker );
 $speakers = rwmb_meta( 'agenda_speakers' );
-$title = rwmb_meta( 'speakers_title' );
 $content = rwmb_meta( 'agenda_wysiwyg' );
 $startTime = rwmb_meta( 'agenda_starttime' );
 $start = date('g:i a', strtotime($startTime));
@@ -10,7 +10,6 @@ $end = date('g:i a', strtotime($endTime));
 $old_date = rwmb_meta( 'agenda_date' );
 $old_date_timestamp = strtotime($old_date);
 $new_date = date('l, F j', $old_date_timestamp);   
-
 
 ?>
 <section class="section-padding">
@@ -23,14 +22,33 @@ $new_date = date('l, F j', $old_date_timestamp);
 				<div class="margin-bottom-60"><?php echo do_shortcode('[addthis]');?></div>
 				<?php echo $content;?>
 
-<ul style="list-style-type: none; padding:0;">
-<li><h5 class="margin-bottom-15 text-uppercase">Speakers</h5></li>
-<?php  foreach ( $speakers as $speaker ) { ?>
-<li><a href="<?php echo get_the_permalink($speaker); ?>"><?php echo get_the_title($speaker); ?></a></li>
-<?php } ?>
-</ul>
+				<?php if ( ! empty( $speakers ) ) { ?>
+				<div class="row">
+					<div class="col-sm-12">
+						<h5 class="margin-bottom-30 text-uppercase">Speakers</h5>
+					</div>
 
+					<?php  foreach ( $speakers as $speaker ) { ?>
+					<?php 
+						$jobtitle = rwmb_meta( 'speakers_title','',$speaker);
+						$headshot = rwmb_meta( 'speakers_speaker_headshot','size=thumbnail',$speaker );
+					?>
 
+					<div class="col-sm-6 col-md-3">
+						<div class="match-height padding-30 margin-bottom-30" style="border:#d4e8f3 solid 4px; position:relative;">
+							<div class="text-center margin-bottom-15">
+								<?php foreach ( $headshot as $image ) { ?> <div style="background-image:url(<?php echo $image['url']; ?>); height:50px; width:50px; background-size:cover; background-position:center center; background-repeat:no-repeat; border-radius:50%; display:inline-block;"></div> <?php } ?>
+							</div>
+							<p class="text-center margin-bottom-0" style="font-size: 14px; font-weight:600;"><?php echo get_the_title($speaker); ?></p>
+							<p class="text-center margin-bottom-0" style="font-size: 14px; font-weight:400; font-style:italic;"><?php echo $jobtitle;?></p>
+							<a href="<?php echo get_the_permalink($speaker); ?>" style="position:absolute; top:0; left:0; width:100%; height:100%; z-index:1;"></a>	
+						</div>
+					</div>
+
+					<?php } ?>
+
+				</div>
+				<? } ?>
 
 			</div>
 		</div>
