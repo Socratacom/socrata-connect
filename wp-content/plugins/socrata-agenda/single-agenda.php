2f5e28b74f7552dd3621120060a99d319cbf2cbc
Michael Church <michael@fishinglounge.com>
2017-03-01 12:47:05 -0500

    progress dev

--- wp-content/plugins/socrata-agenda/single-agenda.php
+++ wp-content/plugins/socrata-agenda/single-agenda.php
@@ -6,55 +6,11 @@ $startTime = rwmb_meta( 'agenda_starttime' );
 $start = date('g:i a', strtotime($startTime));
 $endTime = rwmb_meta( 'agenda_endtime' );
 $end = date('g:i a', strtotime($endTime));
-
 $old_date = rwmb_meta( 'agenda_date' );
 $old_date_timestamp = strtotime($old_date);
 $new_date = date('l, F j', $old_date_timestamp);   
-
 ?>
-<section class="section-padding">
-	<div class="container">
-		<div class="row">
-			<div class="col-sm-10 col-sm-offset-1">
 
-				<h1 class="margin-bottom-15"><?php the_title();?></h1>
-				<p><?php echo $new_date;?> | <?php echo $start;?> - <?php echo $end;?></p>
-				<div class="margin-bottom-60"><?php echo do_shortcode('[addthis]');?></div>
-				<?php echo $content;?>
-
-				<?php if ( ! empty( $speakers ) ) { ?>
-				<div class="row">
-					<div class="col-sm-12">
-						<h5 class="margin-bottom-30 text-uppercase">Speakers</h5>
-					</div>
-
-					<?php  foreach ( $speakers as $speaker ) { ?>
-					<?php 
-						$jobtitle = rwmb_meta( 'speakers_title','',$speaker );
-						$headshot = rwmb_meta( 'speakers_speaker_headshot','size=thumbnail',$speaker );
-						$company = rwmb_meta( 'speakers_company','',$speaker );
-					?>
-
-					<div class="col-sm-6 col-md-3">
-						<div class="match-height padding-30 margin-bottom-30" style="border:#d4e8f3 solid 4px; position:relative;">
-							<div class="text-center margin-bottom-15">
-								<?php foreach ( $headshot as $image ) { ?> <div style="background-image:url(<?php echo $image['url']; ?>); height:50px; width:50px; background-size:cover; background-position:center center; background-repeat:no-repeat; border-radius:50%; display:inline-block;"></div> <?php } ?>
-							</div>
-							<p class="text-center margin-bottom-0" style="font-size: 14px; font-weight:600;"><?php echo get_the_title($speaker); ?></p>
-							<p class="text-center margin-bottom-0" style="font-size: 14px; font-weight:400; font-style:italic; line-height:normal;"><?php echo $jobtitle;?><?php if ( ! empty( $company ) ) { ?>, <?php echo $company;?> <?php };?></p>
-							<a href="<?php echo get_the_permalink($speaker); ?>" style="position:absolute; top:0; left:0; width:100%; height:100%; z-index:1;"></a>	
-						</div>
-					</div>
-
-					<?php } ?>
-
-				</div>
-				<? } ?>
-
-			</div>
-		</div>
-	</div>
-</section>
 <section class="section-padding background-primary-light">
 	<div class="container">
 		<div class="row">
