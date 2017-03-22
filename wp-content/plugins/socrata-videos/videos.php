801c3f86c4d043df014224de905f8429ea30b22c
Michael Church <michael@fishinglounge.com>
2017-03-22 10:37:19 -0400

    progress dev

---
+++ wp-content/plugins/socrata-videos/videos.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: Socrata Videos
+Plugin URI: http://socrata.com/
+Description: This plugin manages videos.
+Version: 1.0
+Author: Michael Church
+Author URI: http://socrata.com/
+License: GPLv2
+*/
