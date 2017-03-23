cfa8ccf1a2d9ce055e20d97a0aac68a8a099b5ea
Michael Church <michael@fishinglounge.com>
2017-03-23 15:30:08 -0400

    progress dev

--- wp-content/plugins/socrata-agenda/single-agenda.php
+++ wp-content/plugins/socrata-agenda/single-agenda.php
@@ -59,15 +59,22 @@ $slides = rwmb_meta( 'agenda_file' );
 							$jobtitle = rwmb_meta( 'speakers_title','',$speaker );
 							$headshot = rwmb_meta( 'speakers_speaker_headshot','size=thumbnail',$speaker );
 							$company = rwmb_meta( 'speakers_company','',$speaker );
+							$bio = rwmb_meta( 'speakers_wysiwyg','',$speaker );
 						?>
 							<div class="col-sm-6 col-md-3">
 								<div class="match-height padding-30 margin-bottom-30" style="border:#d4e8f3 solid 4px; position:relative;">
 									<div class="text-center margin-bottom-15">
-									<?php foreach ( $headshot as $image ) { ?> <div style="background-image:url(<?php echo $image['url']; ?>); height:50px; width:50px; background-size:cover; background-position:center center; background-repeat:no-repeat; border-radius:50%; display:inline-block;"></div> <?php } ?>
+									<?php if ( ! empty( $headshot ) ) { ?> 
+										<?php foreach ( $headshot as $image ) { ?> <div style="background-image:url(<?php echo $image['url']; ?>); height:50px; width:50px; background-size:cover; background-position:center center; background-repeat:no-repeat; border-radius:50%; display:inline-block;"></div> <?php } ?>
+									<?php } else { ?>
+										<div style="background-image:url(/wp-content/uploads/no-image.png); height:50px; width:50px; background-size:cover; background-position:center center; background-repeat:no-repeat; border-radius:50%; display:inline-block;"></div>
+									<?php } ?>									
 									</div>
 									<p class="text-center margin-bottom-0" style="font-size: 14px; font-weight:600;"><?php echo get_the_title($speaker); ?></p>
 									<p class="text-center margin-bottom-0" style="font-size: 14px; font-weight:400; font-style:italic; line-height:normal;"><?php echo $jobtitle;?><?php if ( ! empty( $company ) ) { ?>, <?php echo $company;?> <?php };?></p>
-									<a href="<?php echo get_the_permalink($speaker); ?>" style="position:absolute; top:0; left:0; width:100%; height:100%; z-index:1;"></a>	
+
+									<?php if ( ! empty( $bio ) ) { ?><a href="<?php echo get_the_permalink($speaker); ?>" style="position:absolute; top:0; left:0; width:100%; height:100%; z-index:1;"></a><?php };?>
+									
 								</div>
 							</div>
 
