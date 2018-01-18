3cfc46af663ed462066664b1e36c9ebba71af965
Michael Church <michael@fishinglounge.com>
2018-01-18 15:41:18 -0500

    progress dev

--- wp-content/themes/connect-2018/templates/footer.php
+++ wp-content/themes/connect-2018/templates/footer.php
@@ -2,8 +2,8 @@
   <div class="container-fluid">
   	<div class="row">
   		<div class="col-sm-12">
-  			<p class="text-center"><a href="https://socrata.com" class="footer-logo-white" target="_blank"></a></p>
-  			<p class="text-center mdc-text-blue-grey-100"><small>&copy; <?php echo date("Y");?> Socrata. All rights reserved.<br>Co-hosted by the <a href="#" target="_blank" rel="noopener noreferrer" class="mdc-text-light-blue-200">City of Austin</a>.</small></p>
+  			<p class="text-center"><a href="https://socrata.com" class="footer-logo-white" target="_blank" rel="noopener noreferrer"></a></p>
+  			<p class="text-center mdc-text-blue-grey-100"><small>&copy; <?php echo date("Y");?> Socrata. All rights reserved.<br>Co-hosted by the <a href="https://data.austintexas.gov/" target="_blank" rel="noopener noreferrer" class="mdc-text-light-blue-200">City of Austin</a>.</small></p>
   			<p class="text-center"><a href="/registration" class="btn btn-sm btn-light"><span class="mdc-text-blue-grey-500">Register</span></a></p>
   		</div>
   	</div>
